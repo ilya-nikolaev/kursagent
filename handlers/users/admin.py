9bf3cfe6f1d6279146ee95d8a8ffce889d279128
INik28 <INik28@github.com>
2021-04-27 20:07:55 +0300

    Появилась команда для добавления новых уровней/предметов

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -1,12 +1,38 @@
+import asyncio
+
 from aiogram import types
+from aiogram.dispatcher.filters import IDFilter
 from sqlalchemy.orm import Session
 
+from data.config import ADMINS
 from loader import dp
-from utils.db_api.models import User
+from utils.db_api.models import User, Level, Subject
 
 
-@dp.message_handler(commands=['users'])
+@dp.message_handler(IDFilter(ADMINS), commands=['users'])
 async def admin_menu(message: types.Message, db: Session):
     await message.delete()
     
-    await message.answer(f'Количество пользователей бота: {db.query(User).count()}')
+    msg = await message.answer(f'Количество пользователей бота: {db.query(User).count()}')
+    await asyncio.sleep(5)
+    await msg.delete()
+
+
+@dp.message_handler(IDFilter(ADMINS), commands=['add_level'])
+async def add_level(message: types.Message, db: Session):
+    db.add(Level(name=message.get_args().replace(' ', '_')))
+    db.commit()
+    
+    msg = await message.answer('Принято!')
+    await asyncio.sleep(5)
+    await msg.delete()
+
+
+@dp.message_handler(IDFilter(ADMINS), commands=['add_subject'])
+async def add_subject(message: types.Message, db: Session):
+    db.add(Subject(name=message.get_args().replace(' ', '_')))
+    db.commit()
+
+    msg = await message.answer('Принято!')
+    await asyncio.sleep(5)
+    await msg.delete()
